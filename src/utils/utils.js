8157dc3f3d254b5e7459131bf994b8baffc132d2
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2024-06-26 21:17:46 +0200

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,16 +1,13 @@
-import PropTypes from "prop-types";
-
-export const fetchDataDetails = async (term, setLoading, setError, setData) => {
+export const fetchDataCard = async (setLoading, setError, setData) => {
     setLoading(true);
     setError("");
     try {
-        const url = `https://api.fda.gov/drug/drugsfda.json?search=products.brand_name:"${term}"`;
-        const response = await fetch(url);
+        const response = await fetch('https://api.fda.gov/drug/drugsfda.json?search=products.brand_name:*');
         const responseJson = await response.json();
-        if (responseJson.results && responseJson.results.length > 0) {
-            setData(responseJson.results[0]);
+        if (responseJson.results) {
+            setData(responseJson.results);
         } else {
-            setData(null);
+            setData([]);
         }
     } catch (error) {
         console.error("Error fetching data: ", error);
@@ -18,35 +15,4 @@ export const fetchDataDetails = async (term, setLoading, setError, setData) => {
     } finally {
         setLoading(false);
     }
-};
-
-
-export const fetchDataCard = async (url, setLoading, setError, setData) => {
-    setLoading(true);
-    setError("");
-    try {
-        const url = "https://api.fda.gov/drug/drugsfda.json?count=products.route.exact";
-        const response = await fetch(url);
-        const responseJson = await response.json();
-        setData(responseJson.results);
-    } catch (error) {
-        console.error("Error fetching data: ", error);
-        setError(error.message || "Something went wrong while fetching data");
-    } finally {
-        setLoading(false);
-    }
-};
-
-fetchDataDetails.propTypes = {
-    term: PropTypes.string.isRequired,
-    setLoading: PropTypes.func.isRequired,
-    setError: PropTypes.func.isRequired,
-    setData: PropTypes.func.isRequired,
-};
-
-fetchDataCard.propTypes = {
-    url: PropTypes.string.isRequired,
-    setLoading: PropTypes.func.isRequired,
-    setError: PropTypes.func.isRequired,
-    setData: PropTypes.func.isRequired,
 };
